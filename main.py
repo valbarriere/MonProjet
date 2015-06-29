36921d7b5ad9a6f0db51b8e5bef1f8c17c57d946
lucas <lucasclaude3@gmail.com>
2015-06-29 13:48:05 +0200

    main corrigé

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
+from sets import Set
 from formatage import *
 from extraction import *
 import pycrfsuite
-import cPickle as pickle
+import numpy as np
 
-ALL_LABELS = {'evaluation','affect','source','cible'}
+ALL_LABELS = {'evaluation','affect','source','target'}
 path = "C:/Users/claude-lagoutte/Documents/LUCAS/These/Python/Datasets/Semaine"
 
 #%% Création des dump
@@ -17,7 +18,7 @@ dump_datasetsemaine(path+"/test")
 #%% Extraction des données d'apprentissage
 
 for label in ALL_LABELS:
-    X_train, y_train = extract2CRFsuite(path+"/train/dump",label)    
+    X_train, y_train = extract2CRFsuite(path+"/train2/dump",label)    
     trainer = pycrfsuite.Trainer(verbose=False)
     for xseq, yseq in zip(X_train, y_train):
         trainer.append(xseq, yseq)    
@@ -29,17 +30,17 @@ for label in ALL_LABELS:
         # include transitions that are possible, but not observed
         'feature.possible_transitions': True
     })    
-    trainer.train('basic_model_'+label)
+    trainer.train('basic_model2_'+label)
 
 #%% Tagging exact measure
 
 well_tagged = {}
-pos = [1]*len(X_test)
+neg = {}
 for label in ALL_LABELS:
     tagger = pycrfsuite.Tagger(verbose=False)
-    tagger.open('basic_model_'+label)
+    tagger.open('basic_model2_'+label)
     
-    X_test, y_test = extract2CRFsuite(path+"/test/dump",label)
+    X_test, y_test = extract2CRFsuite(path+"/test2/dump",label)
     
     i=0
     cpt=0
@@ -48,16 +49,16 @@ for label in ALL_LABELS:
         if pred_labels == corr_labels:
             i+=1
         else:
-            pos[cpt] = 0
+            neg[cpt] = 1
         cpt+=1
         
-    well_tagged[label] = str(i/len(X_test) * 100)
+    well_tagged[label] = str(i/cpt * 100)
     print("Well-tagged sentences for label "+label+" : "+well_tagged[label]+" %")
     
-well_tagged['global'] = str(np.sum(pos)/len(X_test) * 100)
-print("Well-tagged sentences : "+well_tagged['global']+"%")
+well_tagged['global'] = str((cpt-len(neg))/cpt * 100)
+print("Well-tagged sentences : "+well_tagged['global']+" %")
 
-f = open('bin_classifiers_results_exact','w')
+f = open('bin_classifiers_results2_exact','w')
 for lab in well_tagged.keys():
     f.write(lab+"\t"+well_tagged[lab])
     f.write('\n')
@@ -67,26 +68,45 @@ f.close()
 #%% Tagging overlap measure
 
 well_tagged = {}
+y_pred = {}
+y_corr = {}
 for label in ALL_LABELS:
     tagger = pycrfsuite.Tagger(verbose=False)
-    tagger.open('basic_model_'+label)
+    tagger.open('basic_model2_'+label)
     
-    X_test, y_test = extract2CRFsuite(path+"/test/dump",label)
+    X_test, y_test = extract2CRFsuite(path+"/test2/dump",label)
     
-    i=0
+    cpt=0
+    nb_words=0
+    nb_pos=0
     for sent, corr_labels in zip(X_test,y_test):
         pred_labels = tagger.tag(sent)
         
-        n=0
         for j in range(len(corr_labels)):
             if pred_labels[j] == corr_labels[j]:
-                n+=1
-        i+= n/len(corr_labels)
-        
-    well_tagged[label] = str(i/len(X_test) * 100)
-    print("Well-tagged sentences for label "+label+" : "+well_tagged[label]+" %")
+                nb_pos+=1
+                
+            if (cpt,j) not in y_pred.keys():
+                y_pred[cpt,j] = [pred_labels[j]]
+                y_corr[cpt,j] = [corr_labels[j]]
+            else:
+                y_pred[cpt,j].append(pred_labels[j])
     
-f = open('bin_classifiers_results_overlap','w')
+        cpt+=1
+        nb_words+=len(corr_labels)
+        
+    well_tagged[label] = str(nb_pos/nb_words * 100)
+    print("Well-tagged tokens for label "+label+" : "+well_tagged[label]+" %")
+
+nb_pos=0
+for key in y_pred.keys():
+    if Set(y_pred[key]) == Set(y_corr[key]):
+        nb_pos+=1
+well_tagged['global'] = str(nb_pos/len(y_pred.keys()) * 100)
+print("Well-tagged tokens : "+well_tagged['global']+" %")
+
+
+f = open('bin_classifiers_results2_overlap','w')
 for lab in well_tagged.keys():
     f.write(lab+"\t"+well_tagged[lab])
     f.write('\n')
